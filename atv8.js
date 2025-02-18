3404fd0f1c7c2d797c7acf77cf749a69818ed6fe
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2025-02-17 21:21:14 -0300

    Create atv8.js

---
+++ atv8.js
@@ -0,0 +1,8 @@
+function exibirMensagem() {
+var nome = document.getElementById('nome').value;
+var cidade = document.getElementById('cidade').value;
+var moraNaCidade = document.querySelector('input[name="cidade"]:checked').value;
+var mensagem = `Bem-vindo(a) ${nome}, a cidade escolhida foi ${cidade}. Você mora na cidade: ${moraNaCidade === 'sim' ? 'sim' : 'não'}.`;
+document.getElementById('mensagem').textContent = mensagem;
+
+}
